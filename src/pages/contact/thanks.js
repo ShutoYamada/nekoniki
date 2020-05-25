b6637fba02676940201428ab07f04a9b53044b05
ShutoYamada <stym1122@gmail.com>
2020-05-25 13:11:51 +0900

    お問い合わせページの表示を変更

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,9 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1 style={{color : '#014C86'}}>Thank You!</h1>
+          <p>ご記入頂いた情報は無事送信されました。</p>
+          <p>お問い合わせ頂き、ありがとうございました。</p>
         </div>
       </div>
     </section>
