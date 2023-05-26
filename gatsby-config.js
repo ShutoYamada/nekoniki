52df39f001e803112e210ec98cdb3eace1a40cbf
ShutoYamada <stym1122@gmail.com>
2023-05-26 17:06:15 +0900

    GA4を設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,72 +2,74 @@ module.exports = {
   // サイトメタデータ
   siteMetadata: {
     // タイトル
-    title: 'ネコニキの開発雑記',
+    title: "ネコニキの開発雑記",
     // 概要
     description:
-      '静岡在住SEがJavascript,React,ReactNative等の開発記事を載せていきます。',
-    siteUrl:
-      `https://nekoniki.com/`
+      "静岡在住SEがJavascript,React,ReactNative等の開発記事を載せていきます。",
+    siteUrl: `https://nekoniki.com/`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-adsense`,
       options: {
-          publisherId: `ca-pub-8245101512786403`
+        publisherId: `ca-pub-8245101512786403`,
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-155134150-2",
+        trackingIds: ["G-J5LVDBEL3V"], // GoogleAnalytics 測定ID
+        pluginConfig: {
+          head: true, // headタグに記載されるようにコンフィグを設定します。
+        },
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     `gatsby-plugin-sitemap`,
-    'gatsby-plugin-robots-txt',
+    "gatsby-plugin-robots-txt",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
+        name: "uploads",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
+        name: "images",
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-prismjs-title',
+            resolve: "gatsby-remark-prismjs-title",
             options: {
-              className: 'your-custom-class-name'
-            }
-          },
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
+              className: "your-custom-class-name",
             },
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
@@ -76,16 +78,16 @@ module.exports = {
             },
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
+              destinationDir: "static",
             },
           },
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'noopener',
+              target: "_blank",
+              rel: "noopener",
             },
           },
           {
@@ -102,18 +104,18 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
-}
+};
